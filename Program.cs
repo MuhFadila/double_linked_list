9598c2e982f2ee6b796f0a0d0be8b3d069a1fda5
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-18 16:49:39 +0700

    create class Node

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@
 
 namespace double_linked_list
 {
+    class Node
+    {
+        /* Node class represent the node of doubly linked list
+         * It consists of the information part and and links to
+         * its succeding and preceeding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
+    }
     class Program
     {
         static void Main(string[] args)
